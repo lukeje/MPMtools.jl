1f04867235103d13fa159a60babeb790bb1f7ef5
Luke Edwards <ledwards@cbs.mpg.de>
2023-04-26 22:54:45 +0200

    Try and improve code readability

--- scripts/optimalParameters.jl
+++ scripts/optimalParameters.jl
@@ -47,27 +47,34 @@ function main()
     TRmin  = parsed_args["TRmin"]
     TRsum  = parsed_args["scantime"]/(2*parsed_args["nlines"])
 
+    # dPD and dR1 have same argument list, so define them here rather than repeating them below
+    dargs(x) = ( MPM.ernst(x[1], x[2], R1), MPM.ernst(x[3], TRsum-x[2], R1), 1.0, 1.0, x[1], x[3], x[2], TRsum-x[2] )
+
+    # choose the fitting function based on which quantitative parameter the output parameters should be optimal for estimating
     if parsed_args["onlyPD"]
-        fitfun = x -> MPM.dPD(MPM.ernst(x[1], x[2], R1), MPM.ernst(x[3], TRsum-x[2], R1), 1.0, 1.0, x[1], x[3], x[2], TRsum-x[2])
+        fitfun = x -> MPM.dPD(dargs(x)...)
         initialoptimum = "PD"
     elseif parsed_args["onlyR1"]
-        fitfun = x -> MPM.dR1(MPM.ernst(x[1], x[2], R1), MPM.ernst(x[3], TRsum-x[2], R1), 1.0, 1.0, x[1], x[3], x[2], TRsum-x[2])
+        fitfun = x -> MPM.dR1(dargs(x)...)
         initialoptimum = "R1"
     elseif parsed_args["both"]
-        fitfun = x -> MPM.dPD(MPM.ernst(x[1], x[2], R1), MPM.ernst(x[3], TRsum-x[2], R1), 1.0, 1.0, x[1], x[3], x[2], TRsum-x[2]) +
-                      MPM.dR1(MPM.ernst(x[1], x[2], R1), MPM.ernst(x[3], TRsum-x[2], R1), 1.0, 1.0, x[1], x[3], x[2], TRsum-x[2])/R1
+        fitfun = x -> MPM.dPD(dargs(x)...) + MPM.dR1(dargs(x)...)/R1
         initialoptimum = "PD"
     end
 
+    # start from optimum for equal TRs
     angles = MPM.optimalVFAangles(TRsum/2, R1, initialoptimum)
 
     opt = optimize(fitfun, [0, 0, 0], [3π/2, TRsum, 3π/2], [angles[1], TRsum/2, angles[2]])
     xopt = opt.minimizer
 
-    print("α1:  $(rad2deg(xopt[1]))°\n")
-    print("TR1: $(1e3*xopt[2]) ms\n")
-    print("α2:  $(rad2deg(xopt[3]))°\n")
-    print("TR2: $(1e3*(TRsum - xopt[2])) ms\n")
+    # precision in output
+    rounddigit(x) = round(x; digits=2)
+
+    println("α1:  $(rounddigit(rad2deg(xopt[1])))°")
+    println("TR1: $(rounddigit(1e3*xopt[2])) ms")
+    println("α2:  $(rounddigit(rad2deg(xopt[3])))°")
+    println("TR2: $(rounddigit(1e3*(TRsum - xopt[2]))) ms")
 
     @assert all([xopt[2], (TRsum - xopt[2])] .≥ TRmin) "One or both TRs were less than TRmin. Please relax your input parameters and try again."
 
