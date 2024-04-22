f01b754d7177d233f7094751b07d9ee8a9f10033
Luke Edwards <ledwards@cbs.mpg.de>
2024-04-22 16:56:34 +0200

    Improve test documentation

--- test/MRImaps_errormaps_tests.jl
+++ test/MRImaps_errormaps_tests.jl
@@ -2,11 +2,9 @@ using MPMtools.MRIutils
 using MPMtools.MRItypes
 using MPMtools.MRImaps
 
-# Test MPM pipeline using synthetic data
-A      = 1000.0
+# Test error map computation using synthetic data
 R1     = 1.0e-3
 R2star = 30.0e-3
-B1     = 1.0
 
 TR     = 45e-3
 TElist = (2:5:40) .* 1e-3
@@ -14,7 +12,7 @@ TElist = (2:5:40) .* 1e-3
 αErnst = MRIutils.ernstangle(TR,R1)
 
 ϵRepeat = MRImaps.dR2star(R1,R2star,[1.0,1.0,1.0],vcat(α...,last(α)),fill(TR,3),fill(TElist,3))
-ϵErnst  = MRImaps.dR2star(R1,R2star,[1.0,1.0,1.0],vcat(α...,αErnst),fill(TR,3),fill(TElist,3))
+ϵErnst  = MRImaps.dR2star(R1,R2star,[1.0,1.0,1.0],vcat(α...,αErnst), fill(TR,3),fill(TElist,3))
 
 # adding Ernst angle measurement should improve R2star estimate
 @test first(ϵErnst) < first(ϵRepeat)
